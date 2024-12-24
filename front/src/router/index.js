591d366b5aca61bd7b8bea68b04f3992d70fd1a0
ILIA TISKIN <tiskin2308@gmail.com>
2024-12-24 23:15:01 +0100

    feat: added CORS to backend, added frontend form

---
+++ front/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../components/HomePage.vue';
+import HelloWorld from '../components/HelloWorld.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  },
+  {
+    path: '/hello',
+    name: 'HelloWorld',
+    component: HelloWorld,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
